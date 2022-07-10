71e7c887c3f9ba0ff63402b711131a37f56c92c5
cybwan <baili@flomesh.io>
2022-07-10 13:13:03 +0800

    Fix forward error.

--- scripts/port-forward-jaeger.sh
+++ scripts/port-forward-jaeger.sh
@@ -7,7 +7,7 @@ source .env
 
 OSM_POD=$(kubectl get pods -n "$K8S_NAMESPACE" --no-headers  --selector app=jaeger | awk 'NR==1{print $1}')
 
-if [ -z "$POD" ]; then
+if [ -z "$OSM_POD" ]; then
     echo "Not found pod: jaeger"
     exit 1
 fi
